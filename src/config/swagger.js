9d3a696d39dcea2f371278d3779d2d1163eb4dd3
samiKechiche <kechichesami@gmail.com>
2026-07-20 11:39:50 +0100

    fixed swagger double-prefix error

--- src/config/swagger.js
+++ src/config/swagger.js
@@ -18,7 +18,7 @@ const options = {
     },
     servers: [
       {
-        url: `http://localhost:${process.env.PORT || 3000}/api/v1`,
+        url: `http://localhost:${process.env.PORT || 3000}`,
         description: 'Development server',
       },
     ],
